f9de6b452fa1966636e9d054e515ffb6ba207341
xiaosun <chsx.sun@mail.utoronto.ca>
2020-11-27 08:25:18 +0800

    small change in server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     //res.send('This should be the root route!')
 
     //sending some HTML
-    res.send('<h1>Please visit examples.html!</h1>')
+    res.send('<h1>Please visit /examples.html!</h1>')
 })
 
 // Error codes
